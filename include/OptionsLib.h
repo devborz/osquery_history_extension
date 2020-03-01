0fcccabd3b80c1e73fdcc41633bd98aaf7c1d3c9
Usman Turkaev <u.turkaev@yandex.ru>
2020-03-01 23:47:24 +0300

    Update OptionsLib

--- include/OptionsLib.h
+++ include/OptionsLib.h
@@ -2,35 +2,24 @@
 
 #include <boost/program_options.hpp>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
 
 namespace bpo = boost::program_options;
 
-enum Option {
-    all,
-    filesystem,
-    bash,
-    vim,
-    help
-};
+enum Option { all, filesystem, bash, vim, help };
 
 class Options {
-public:
-    static Option parse(const std::string&);
+ public:
+  static Option parse(const std::string&);
 
+  static void getOptions(bpo::options_description&);
 
-    static void getOptions(bpo::options_description&);
+  static void notifyOptionError(const std::string&);
 
+  static void getHelp(bpo::options_description& desc);
 
-    static void notifyOptionError(const std::string&);
-
-
-    static void getHelp(bpo::options_description& desc);
-
-
-    static std::shared_ptr<std::vector<Option>> readOption(
-                                            const bpo::variables_map&,
-                                            unsigned int&);
+  static std::shared_ptr<std::vector<Option>> readOption(
+      const bpo::variables_map&, unsigned int&);
 };
--- sources/OptionsLib.cpp
+++ sources/OptionsLib.cpp
@@ -1,69 +1,60 @@
 #include "OptionsLib.h"
 
 Option Options::parse(const std::string& option) {
-    std::map<std::string, Option> optionsMap {
-        std::make_pair("all", all),
-        std::make_pair("filesystem", filesystem),
-        std::make_pair("bash", bash),
-        std::make_pair("vim", vim),
-        std::make_pair("help", help)
-    };
+  std::map<std::string, Option> optionsMap{
+      std::make_pair("all", all), std::make_pair("filesystem", filesystem),
+      std::make_pair("bash", bash), std::make_pair("vim", vim),
+      std::make_pair("help", help)};
 
-    return optionsMap[option];
+  return optionsMap[option];
 }
 
 void Options::notifyOptionError(const std::string& option) {
-    std::cerr << "Option \'" << option << "\' does not exist. See 'help'\n";
+  std::cerr << "Option \'" << option << "\' does not exist. See 'help'\n";
 }
 
 void Options::getHelp(bpo::options_description& desc) {
-    std::cout << "\nOptions:\n" << desc << std::endl;
+  std::cout << "\nOptions:\n" << desc << std::endl;
 }
 
 std::shared_ptr<std::vector<Option>> Options::readOption(
-                                        const bpo::variables_map& vm,
-                                        unsigned int& ec) {
-    std::string option_;
-    std::shared_ptr<std::vector <Option>> enabledOptions(new
-                                                        std::vector<Option>());
-    if (vm.count("help")) {
-        Option option = help;
-        enabledOptions.get()->push_back(option);
+    const bpo::variables_map& vm, unsigned int& ec) {
+  std::string option_;
+  std::shared_ptr<std::vector<Option>> enabledOptions(
+      new std::vector<Option>());
+  if (vm.count("help")) {
+    Option option = help;
+    enabledOptions.get()->push_back(option);
+  } else if (vm.count("all")) {
+    Option option = all;
+    enabledOptions.get()->push_back(option);
+  } else {
+    if (vm.count("filesystem")) {
+      Option option = filesystem;
+      enabledOptions.get()->push_back(option);
     }
-    else if (vm.count("all")) {
-        Option option = all;
-        enabledOptions.get()->push_back(option);
+    if (vm.count("bash")) {
+      Option option = bash;
+      enabledOptions.get()->push_back(option);
     }
-    else {
-        if (vm.count("filesystem")) {
-            Option option = filesystem;
-            enabledOptions.get()->push_back(option);
-        }
-        if (vm.count("bash")) {
-            Option option = bash;
-            enabledOptions.get()->push_back(option);
-        }
-        if (vm.count("vim")) {
-            Option option = vim;
-            enabledOptions.get()->push_back(option);
-        }
+    if (vm.count("vim")) {
+      Option option = vim;
+      enabledOptions.get()->push_back(option);
+    }
+  }
 
-    }
+  if (enabledOptions.get()->empty()) {
+    notifyOptionError(" ");
+    ec = 1;
+  }
 
-    if (enabledOptions.get()->empty()) {
-        notifyOptionError(" ");
-        ec = 1;
-    }
-
-    return enabledOptions;
+  return enabledOptions;
 }
 
 void Options::getOptions(bpo::options_description& desc) {
-    desc.add_options()
-        ("help", "outputs help message")
-        ("filesystem", "updates filesystem's history")
-        ("bash", "updates bash history's file")
-        ("vim", "updates vim history's files")
-        ("all", "updates all history's files")
-    ;
+  desc.add_options()("help", "outputs help message")(
+      "filesystem", "updates filesystem's history")(
+      "bash", "updates bash history's file")(
+      "vim", "updates vim history's files")("all",
+                                            "updates all history's files");
 }
