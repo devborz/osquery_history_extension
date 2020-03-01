4e4cfcd4a6746c1fad78361568f82832060c0a9d
Usman Turkaev <u.turkaev@yandex.ru>
2020-03-01 23:48:32 +0300

    Update FilesLib

--- include/FilesLib.h
+++ include/FilesLib.h
@@ -1,18 +1,18 @@
 #pragma once
 
 #include <boost/filesystem.hpp>
-#include <vector>
-#include <string>
 #include <chrono>
 #include <iomanip>
 #include <iostream>
+#include <string>
+#include <vector>
 #include "ChangedFile.h"
 
 namespace fs = boost::filesystem;
 
 class Files {
-public:
-    static void pushToList(const fs::path&, std::vector<ChangedFile>&);
+ public:
+  static void pushToList(const fs::path&, std::vector<ChangedFile>&);
 
-    static void sortByTime(std::vector<ChangedFile>&);
+  static void sortByTime(std::vector<ChangedFile>&);
 };
--- sources/FilesLib.cpp
+++ sources/FilesLib.cpp
@@ -1,35 +1,27 @@
 #include "FilesLib.h"
 
 void Files::pushToList(const fs::path& pathToObj,
-            std::vector<ChangedFile>& recentlyChangedFiles) {
+                       std::vector<ChangedFile>& recentlyChangedFiles) {
+  auto fileLastChange = fs::last_write_time(pathToObj);
 
-    auto fileLastChange = fs::last_write_time(pathToObj);
+  std::time_t fileLastChangeTime = fileLastChange;
 
-    std::time_t fileLastChangeTime = fileLastChange;
-
-    recentlyChangedFiles.push_back(ChangedFile(pathToObj.filename().string(),
-                                               pathToObj.parent_path(),
-                                               fileLastChangeTime));
+  recentlyChangedFiles.push_back(ChangedFile(pathToObj.filename().string(),
+                                             pathToObj.parent_path(),
+                                             fileLastChangeTime));
 }
 
 void Files::sortByTime(std::vector<ChangedFile>& recentlyChangedFiles) {
+  auto size = recentlyChangedFiles.size();
 
-    auto size = recentlyChangedFiles.size();
-
-    for (unsigned int i = 0; i < size - 1; i++) {
-
-        for (unsigned int j = 0; j < size - i - 1; j++) {
-
-            if (recentlyChangedFiles[j].getTime() >
-                recentlyChangedFiles[j + 1].getTime()) {
-
-                auto temp = recentlyChangedFiles[j];
-                recentlyChangedFiles[j] = recentlyChangedFiles[j + 1];
-                recentlyChangedFiles[j + 1] = temp;
-
-            }
-
-        }
-
+  for (unsigned int i = 0; i < size - 1; i++) {
+    for (unsigned int j = 0; j < size - i - 1; j++) {
+      if (recentlyChangedFiles[j].getTime() >
+          recentlyChangedFiles[j + 1].getTime()) {
+        auto temp = recentlyChangedFiles[j];
+        recentlyChangedFiles[j] = recentlyChangedFiles[j + 1];
+        recentlyChangedFiles[j + 1] = temp;
+      }
     }
+  }
 }
